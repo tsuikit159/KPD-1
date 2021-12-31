f218903d9a22a058ea0272c2bc424007669c8ae6
lonnstyle <lonnstyle@gmail.com>
2021-12-31 15:28:48 +0800

    init repo
    feat: player controller
    feat: player assets

---
+++ unity/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Move the player character
+        controller.Move(horizontalMove,verticalMove);
+    }
+}
