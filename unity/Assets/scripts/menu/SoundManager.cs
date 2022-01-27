f80680e43952903ab1ab8b4662c422957a61a8d4
lonnstyle <lonnstyle@gmail.com>
2022-01-27 12:00:41 +0800

    feat: audio volume config

---
+++ unity/Assets/scripts/menu/SoundManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("audioVolume"))
+        {
+            PlayerPrefs.SetFloat("audioVolume", 0.5f);
+        }
+        Load();
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("audioVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("audioVolume", volumeSlider.value);
+    }
+}
